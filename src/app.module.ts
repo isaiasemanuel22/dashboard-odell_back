6af0397e8551770b957484fed1baba8814273c52
isaiasemanuel22 <isaiasemanuelcalfin@hotmail.com>
2025-03-08 12:15:29 -0300

    conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { BillModule } from './bills/bill.module';
 import { ConfigGeneralModule } from './config/config.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
+  controllers:[AppController],
   imports: [
     ProductsModule,
     FixedExpenseModule,
     BillModule,
-    AppController,
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: (process.env.DB_TYPE ?? 'mysql') as 'mysql',
@@ -31,7 +32,11 @@ import { AppController } from './app.controller';
       isGlobal: true, // Asegúrate de que las variables estén disponibles globalmente
     }),
     ConfigGeneralModule,
+
   ],
+  providers:[
+    AppService
+  ]
 })
 export class AppModule {
   constructor() {}
