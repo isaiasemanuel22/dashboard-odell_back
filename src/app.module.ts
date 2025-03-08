6960641729220e4aebec3449fd451a4123d174ab
isaiasemanuel22 <isaiasemanuelcalfin@hotmail.com>
2025-03-08 12:53:34 -0300

    conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { AppService } from './app.service';
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME,
-        entities: [__dirname + '/dist/**/*.entity{.ts,.js}'],
+        entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: false,
       }),
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Product } from '../../src/commons/models/Product.entity';
-import { ProductInfo } from '../../src/commons/models/ProductsInfo.entity';
+import { Product } from '../commons/models/Product.entity';
+import { ProductInfo } from '../commons/models/ProductsInfo.entity';
 
 import { ProductsController } from './controllers/products.controller';
 import { ProductsOrmService } from './services/products.orm.service';
 
 import { ColorsService } from './services/colors/colors.service';
 import { ProductsInfoOrmService } from './services/products-info.orm/products-info.orm.service';
-import { BillModule } from '../../src/bills/bill.module';
-import { FilamentModule } from '../../src/filament/filament.module';
+import { BillModule } from '../bills/bill.module';
+import { FilamentModule } from '../filament/filament.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product,ProductInfo]), BillModule,FilamentModule],
